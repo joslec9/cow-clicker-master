a7a31626ab28044ffc16bfbeb942d3fd0bb6e48f
Jose Lechuga <joslec9@gmail.com>
2015-12-27 02:31:46 -0500

    feat: Cow Clicker
    A simple webpage that has a cow image in the middle (centered
    horizontally on the page) and a counter label below it. Every time you
    click the cow image, the counter is incremented by 1. The counter starts
    with a value of 0.

---
+++ README.md
@@ -0,0 +1,8 @@
+Cow Clicker
+===========
+
+To use this application:
+1) download file and open index.html in browser.
+2) once open in browser simply click on Cow image and the counter will increase every time you click on the image.
+
+Source: here is a URL for a cow image, https://upload.wikimedia.org/wikipedia/commons/2/21/Cow_cartoon_04.svg,
---
+++ js/clicker.js
@@ -0,0 +1,54 @@
+// Make a canvas element
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+// Make a cow image object
+var cowReady = false;
+var cow = new Image();
+cow.onload = function () {
+    cowReady = true;
+};
+cow.src = "images/Cow_cartoon_04.svg";
+
+// Set up counting clicks
+var clicks = 0;
+var incrementClicks = function () {
+    clicks ++;
+};
+addEventListener("mousedown", incrementClicks, false);
+
+// Draw everything
+var render = function () {
+    ctx.clearRect(0,0,512,480);
+	if (cowReady) {
+		ctx.drawImage(cow, 0, 0);
+        console.log('Cow ready');
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Clicks: " + clicks, 150, 460);
+};
+
+var reset = function () {
+    clicks = 0;
+};
+
+var main = function () {
+	render();
+    // Do it again!
+    requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+reset();
+main();
